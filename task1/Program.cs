debc51e9d399f7dd959f0fb4fdc80ee804c2e838
Алексей Шепелев <distort9@mail.ru>
2024-01-13 14:14:13 +0300

    формирование массивов

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] arr = new int[5];
+arr[0] = 2;
+arr[1] = 1;
+arr[2] = 7;
+arr[3] = 5;
+arr[4] = 9;
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+
+int [] arr2 = new int[5] {1, 4, 5, 2, 0};
+int [] arr3 = {2, 4, 3, 7, 0};
